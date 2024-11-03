a3f84ca5ca3aecc76d2a4ed65975ad9de44cb0c8
Simon Branch <simonmbranch@gmail.com>
2024-11-02 22:31:41 -0700

    Add frontend and many other tweaks

---
+++ src/main/kotlin/Backend.kt
@@ -0,0 +1,175 @@
+package com.sigxcpu.frcproj
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardCopyOption
+import java.nio.file.attribute.PosixFilePermissions
+import kotlin.io.path.*
+
+fun createProject(resourcesPath: Path, destinationPath: Path,
+                  config: ProjectConfig,
+                  teamNumber: Int) {
+
+    // See vscode-wpilib/src/shared/exampletemplateapi.ts
+    // vscode-wpilib/src/shared/generator.ts
+    // vscode-wpilib/src/shared/templates.ts
+
+    // Helpers for constructing Paths relative to the resources directory
+    // and the destination directory.
+    fun resource(first: String, vararg rest: String) =
+        resourcesPath.resolve(Path.of(first, *rest))
+    fun destination(first: String, vararg rest: String) =
+        destinationPath.resolve(Path.of(first, *rest))
+
+    copyCodeTree(
+        resource("java", "src", when (config.projectType) {
+            ProjectType.Template -> "templates"
+            ProjectType.Example -> "examples"
+        }, config.name),
+        destination("src", "main", "java", "frc", "robot")
+    )
+
+    if (config.hasUnitTests)
+        copyCodeTree(
+            resource("java", "src", when (config.projectType) {
+                ProjectType.Template -> "templates_test"
+                ProjectType.Example -> "examples_test"
+            }, config.name),
+            destination("src", "test", "java", "frc", "robot")
+        )
+
+    copyGradleTree(
+        resource("gradle", config.gradleBase),
+        destinationPath
+    )
+
+    copyGradleTree(
+        resource("gradle", "shared"),
+        destinationPath
+    )
+
+    setExecutableBit(destination("gradlew"))
+
+    patchGradleBuild(
+        resource("gradle", "version.txt"),
+        destination("build.gradle")
+    )
+
+    createDeployDirectory(destination("src", "main", "deploy"))
+
+    copyVendordeps(
+        resource("vendordeps"),
+        destination("vendordeps"),
+        extraVendordeps = config.extraVendordeps
+    )
+
+    setTeamNumber(destination(".wpilib", "wpilib_preferences.json"),
+        teamNumber)
+}
+
+private fun copyCodeTree(sourceDir: Path, destDir: Path) {
+    Files.walk(sourceDir).forEach { sourcePath ->
+        val destinationPath = destDir.resolve(sourceDir.relativize(sourcePath))
+        if (sourcePath.isDirectory()) {
+            destinationPath.createDirectories()
+        } else if (sourcePath.isRegularFile()) {
+            if (sourcePath.name.endsWith(".gradle") || sourcePath.name.endsWith(".java")) {
+                copyAndProcessCodeFile(sourcePath, destinationPath)
+            } else {
+                Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING)
+            }
+        }
+    }
+}
+
+private val packageRegex = Regex("""edu\.wpi\.first\.wpilibj\.(?:examples|templates)\.[^.;]+""")
+private val packageReplacement = "frc.robot"
+
+private fun copyAndProcessCodeFile(sourcePath: Path, destinationPath: Path) {
+    sourcePath.toFile().bufferedReader().use { reader ->
+        destinationPath.toFile().bufferedWriter().use { writer ->
+            reader.lineSequence().forEach { line ->
+                // Replace edu.wpi.first... package declarations with the default
+                // frc.robot ones.
+                writer.write(line.replace(packageRegex, packageReplacement))
+                writer.newLine()
+            }
+        }
+    }
+}
+
+private fun copyGradleTree(sourceDir: Path, destinationDir: Path) {
+    // Java's Files.walk() doesn't have the ability to prune subdirectories, so
+    // we use Kotlin's File.walk() instead.  Note that it creates File objects
+    // instead of Paths.
+    sourceDir.toFile().walk().onEnter { file ->
+        // Skip over "bin" and ".project" files.  Technically, in the original code,
+        // "bin" is only skipped at the top level.  But no WPILib template should
+        // have a nested "bin" folder inside a gradle folder that they expect to
+        // be copied over.  Also, I'm not sure whether the ".project" matching
+        // (which uses .indexOf() on the full path) was intended to match files like
+        // "choreo.project", but I'm reproducing their behavior.
+        file.name != "bin" && !file.name.contains(".project")
+    }.forEach { sourceFile ->
+        val sourcePath = sourceFile.toPath()
+        val destinationPath = destinationDir.resolve(sourceDir.relativize(sourcePath))
+        if (sourcePath.isDirectory()) {
+            destinationPath.createDirectories()
+        } else if (sourcePath.isRegularFile()) {
+            Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING)
+        }
+    }
+}
+
+private fun setExecutableBit(path: Path) {
+    val permissions = Files.getPosixFilePermissions(path).toMutableSet()
+    permissions.addAll(PosixFilePermissions.fromString("--x--x--x"))
+    Files.setPosixFilePermissions(path, permissions)
+}
+
+private val mainClassRegex = "###ROBOTCLASSREPLACE###"
+private val mainClassReplacement = "frc.robot.Main"
+private val gradleVersionRegex = "###GRADLERIOREPLACE###"
+
+private fun patchGradleBuild(gradleVersionPath: Path, gradleBuild: Path) {
+    val version = gradleVersionPath.readText().trim()
+    val text = gradleBuild.readText()
+        .replace(mainClassRegex, mainClassReplacement)
+        .replace(gradleVersionRegex, version)
+    gradleBuild.writeText(text)
+}
+
+private fun createDeployDirectory(destinationDir: Path) {
+    destinationDir.createDirectories()
+    destinationDir.resolve("example.txt").writeText(
+        """
+        Files placed in this directory will be deployed to the RoboRIO into the
+        'deploy' directory in the home folder. Use the 'Filesystem.getDeployDirectory' wpilib function
+        to get a proper path relative to the deploy directory.
+        """.trimIndent()
+    )
+}
+
+private val vendordepNamesToFiles = mapOf(
+    "romi" to "RomiVendordep.json",
+    "xrp" to "XRPVendordep.json",
+)
+
+private fun copyVendordeps(sourceDir: Path, destinationDir: Path,
+                           extraVendordeps: List<String>) {
+    destinationDir.createDirectories()
+    for (dep in listOf("WPILibNewCommands.json")
+            + extraVendordeps.map(vendordepNamesToFiles::getValue))
+    {
+        Files.copy(sourceDir.resolve(dep), destinationDir.resolve(dep),
+            StandardCopyOption.REPLACE_EXISTING)
+    }
+}
+
+private val teamNumberRegex = Regex("-1$", options = setOf(RegexOption.MULTILINE))
+
+private fun setTeamNumber(destinationPath: Path, teamNumber: Int) {
+    val text = destinationPath.readText()
+        .replace(teamNumberRegex, teamNumber.toString())
+    destinationPath.writeText(text)
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,193 +1,168 @@
 package com.sigxcpu.frcproj
 
+import com.beust.klaxon.Klaxon
 import java.nio.file.Files
 import java.nio.file.Path
-import java.nio.file.StandardCopyOption
-import java.nio.file.attribute.PosixFilePermissions
 import kotlin.io.path.*
 
-var lineChangeCount = 0
-
-enum class Blueprint(val codeDirectory: String, val testsDirectory: String) {
-    Template("templates", "templates_test"),
-    Example("examples", "examples_test")
-}
-
-fun main() {
-    createProject(
-        resourcesPath = Path.of("/home/simon/ext/vscode-wpilib/vscode-wpilib/resources"),
-        destinationPath = Path.of("/tmp/test"),
-        name = "romicommandbased",
-        blueprint = Blueprint.Template,
-        gradleBase = "javaromi",
-        extraVendordeps = arrayOf("romi"),
-        hasUnitTests = false
-    )
-    println("Patched ${lineChangeCount} of package declarations.")
-}
-
-fun createProject(resourcesPath: Path, destinationPath: Path,
-                  name: String,
-                  blueprint: Blueprint,
-                  gradleBase: String,
-                  extraVendordeps: Array<String>,
-                  hasUnitTests: Boolean) {
-
-    // See vscode-wpilib/src/shared/exampletemplateapi.ts
-    // vscode-wpilib/src/shared/generator.ts
-    // vscode-wpilib/src/shared/templates.ts
-
-    fun source(first: String, vararg rest: String) = resourcesPath.resolve(
-        Path.of(first, *rest)
-    )
-    fun destination(first: String, vararg rest: String) = destinationPath.resolve(
-        Path.of(first, *rest)
-    )
-
-    copyCodeTree(
-        source("java", "src", blueprint.codeDirectory, name),
-        destination("src", "main", "java", "frc", "robot")
-    )
-
-    if (hasUnitTests) {
-        copyCodeTree(
-            source("java", "src", blueprint.testsDirectory, name),
-            destination("src", "test", "java", "frc", "robot")
-        )
-    }
-
-    copyGradleTree(
-        source("gradle", gradleBase),
-        destinationPath
-    )
-
-    copyGradleTree(
-        source("gradle", "shared"),
-        destinationPath
-    )
-
-    setExecutableBit(destination("gradlew"))
-
-    patchGradleBuild(
-        source("gradle", "version.txt").readText().trim(),
-        destination("build.gradle")
-    )
-
-    createDeployDirectory(destination("src", "main", "deploy"))
-
-    copyVendordeps(
-        source("vendordeps"),
-        destination("vendordeps"),
-        depFiles = listOf("WPILibNewCommands.json")
-                + extraVendordeps.map(vendordepNamesToFiles::getValue)
-    )
-
-    setTeamNumber(destination(".wpilib", "wpilib_preferences.json"), 1778)
-}
-
-fun copyCodeTree(sourceDir: Path, destDir: Path) {
-    Files.walk(sourceDir).forEach { sourcePath ->
-        val destinationPath = destDir.resolve(sourceDir.relativize(sourcePath))
-        if (sourcePath.isDirectory()) {
-            destinationPath.createDirectories()
-        } else if (sourcePath.isRegularFile()) {
-            if (sourcePath.name.endsWith(".gradle") || sourcePath.name.endsWith(".java")) {
-                copyAndProcessCodeFile(sourcePath, destinationPath)
-            } else {
-                Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING)
-            }
-        }
-    }
-}
-
-val packageRegex = Regex("""edu\.wpi\.first\.wpilibj\.(?:examples|templates)\.[^.;]+""")
-val packageReplacement = "frc.robot"
-
-fun copyAndProcessCodeFile(sourcePath: Path, destinationPath: Path) {
-    sourcePath.toFile().bufferedReader().use { reader ->
-        destinationPath.toFile().bufferedWriter().use { writer ->
-            reader.lineSequence().forEach { line ->
-                // Replace edu.wpi.first... package declarations with the default
-                // frc.robot ones.
-                val processedLine = line.replace(packageRegex, packageReplacement)
-                if (processedLine != line)
-                    lineChangeCount++
-                writer.write(processedLine)
-                writer.newLine()
-            }
-        }
-    }
-}
-
-fun copyGradleTree(sourceDir: Path, destinationDir: Path) {
-    // Java's Files.walk() doesn't have the ability to prune subdirectories, so
-    // we use Kotlin's File.walk() instead.  Note that it creates File objects
-    // instead of Paths.
-    sourceDir.toFile().walk().onEnter { file ->
-        // Skip over "bin" and ".project" files.  Technically, in the original code,
-        // "bin" is only skipped at the top level.  But no WPILib template should
-        // have a nested "bin" folder inside a gradle folder that they expect to
-        // be copied over.  Also, I'm not sure whether the ".project" matching
-        // (which uses .indexOf() on the full path) was intended to match files like
-        // "choreo.project", but I'm reproducing their behavior.
-        file.name != "bin" && !file.name.contains(".project")
-    }.forEach { sourceFile ->
-        val sourcePath = sourceFile.toPath()
-        val destinationPath = destinationDir.resolve(sourceDir.relativize(sourcePath))
-        if (sourcePath.isDirectory()) {
-            destinationPath.createDirectories()
-        } else if (sourcePath.isRegularFile()) {
-            Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING)
-        }
-    }
-}
-
-fun setExecutableBit(path: Path) {
-    val permissions = Files.getPosixFilePermissions(path).toMutableSet()
-    permissions.addAll(PosixFilePermissions.fromString("--x--x--x"))
-    Files.setPosixFilePermissions(path, permissions)
-}
-
-val mainClassRegex = "###ROBOTCLASSREPLACE###"
-val mainClassReplacement = "frc.robot.Main"
-val gradleVersionRegex = "###GRADLERIOREPLACE###"
-
-fun patchGradleBuild(gradleVersion: String, gradleBuild: Path) {
-    val text = gradleBuild.toFile().readText()
-    val patchedText = text
-        .replace(mainClassRegex, mainClassReplacement)
-        .replace(gradleVersionRegex, gradleVersion)
-    gradleBuild.toFile().writeText(patchedText)
-}
-
-val exampleDeployText = """
-Files placed in this directory will be deployed to the RoboRIO into the
-'deploy' directory in the home folder. Use the 'Filesystem.getDeployDirectory' wpilib function
-to get a proper path relative to the deploy directory.
-""".trimIndent()
-
-fun createDeployDirectory(destinationDir: Path) {
-    destinationDir.createDirectories()
-    destinationDir.resolve("example.txt").writeText(exampleDeployText)
-}
-
-val vendordepNamesToFiles = mapOf(
-    "romi" to "RomiVendordep.json",
-    "xrp" to "XRPVendordep.json",
+enum class ProjectType { Template, Example }
+data class ProjectConfig(
+    val name: String,
+    val projectType: ProjectType,
+    val gradleBase: String,
+    val extraVendordeps: List<String>,
+    val hasUnitTests: Boolean
 )
 
-fun copyVendordeps(sourceDir: Path, destinationDir: Path, depFiles: List<String>) {
-    destinationDir.createDirectories()
-    for (dep in depFiles) {
-        Files.copy(sourceDir.resolve(dep), destinationDir.resolve(dep),
-            StandardCopyOption.REPLACE_EXISTING)
+data class ProjectJson(
+    val name: String,
+    val description: String,
+    val tags: ArrayList<String>,
+    val foldername: String,
+    val gradlebase: String,
+    val hasunittests: Boolean = false,
+    val mainclass: String,
+    val commandversion: Int = 2,
+    val extravendordeps: ArrayList<String> = arrayListOf(),
+)
+
+fun main() {
+    val resourcesPath = chooseResourcesPath()
+    val destinationPath = chooseDestinationPath()
+    val templateConfig = chooseTemplate(resourcesPath)
+    val teamNumber = chooseTeamNumber()
+    createProject(resourcesPath, destinationPath, templateConfig, teamNumber)
+    println("project successfully created")
+}
+
+private fun tildeExpand(s: String): String {
+    return if (s.startsWith("~"))
+        s.replaceFirst("~", System.getProperty("user.home"))
+    else s
+}
+
+private fun chooseResourcesPath(): Path {
+    val defaultPaths = mutableListOf<Path>()
+    val wpilib = Path.of(System.getProperty("user.home"), "wpilib")
+    if (wpilib.isDirectory()) {
+        for (subdir in Files.newDirectoryStream(wpilib, Path::isDirectory)) {
+            val y: Int? = subdir.fileName.toString().toIntOrNull()
+            if (y == null)
+                continue
+            val path = subdir.resolve(
+                Path.of("utility", "resources", "app", "resources")
+            )
+            if (path.exists())
+                defaultPaths.add(path)
+        }
+    }
+    fun pathLooksValid(p: Path): Boolean {
+        if (!p.exists()) {
+            println("that path doesn't exist!")
+            return false
+        }
+        if (!p.isDirectory()) {
+            println("that path isn't a directory")
+            return false
+        }
+        if (!p.resolve(Path.of("gradle", "version.txt")).exists()) {
+            println("that doesn't look valid (no gradle/version.txt)")
+            return false
+        }
+        return true
+    }
+    if (defaultPaths.size > 0) {
+        println("the following resource paths were detected:")
+        for (idx in defaultPaths.indices)
+            println("  [${idx+1}] ${defaultPaths[idx]}")
+        while (true) {
+            print("what path would you like to use? (enter one of the numbers above or your own path) ")
+            val response = tildeExpand(readln())
+            var num = response.toIntOrNull()
+            if (response == "") num = 1
+            if (num != null) {
+                if (num - 1 in defaultPaths.indices)
+                    return defaultPaths[num - 1]
+                println("number out of range, try again")
+                continue
+            }
+            val path = Path.of(response)
+            if (pathLooksValid(path))
+                return path
+        }
+    } else {
+        while (true) {
+            print("what path would you like to use? ")
+            val path = Path.of(tildeExpand(readln()))
+            if (pathLooksValid(path))
+                return path
+        }
     }
 }
 
-val teamNumberRegex = Regex("-1$", options = setOf(RegexOption.MULTILINE))
+private fun chooseTemplate(resourcesPath: Path): ProjectConfig {
+    val jsonText = resourcesPath.resolve(
+        Path.of("java", "src", "templates", "templates.json")
+    ).readText()
+    val jsons = Klaxon().parseArray<ProjectJson>(jsonText)!!
+        .sortedBy { it.foldername }
+    println("the following templates are available:")
+    for (j in jsons) {
+        println("  [${j.foldername}]: ${j.description}")
+    }
+    lateinit var json: ProjectJson
+    while (true) {
+        try {
+            print("which template would you like to use? ")
+            val template = readln()
+            json = jsons.first { it.foldername.startsWith(template) }
+            if (json.foldername != template)
+                println("using template ${json.foldername}")
+            break
+        } catch (e: NoSuchElementException) {
+            println("invalid name, try again")
+        }
+    }
+    // Expectations for the template files; these are true in the current
+    // wpilib repository, but there's code to handle other situations in
+    // VSCode that aren't implemented here.
+    require(json.mainclass == "Main" && json.commandversion == 2)
+    return ProjectConfig(
+        name = json.foldername,
+        projectType = ProjectType.Template,
+        gradleBase = json.gradlebase,
+        extraVendordeps = json.extravendordeps,
+        hasUnitTests = json.hasunittests
+    )
+}
 
-fun setTeamNumber(destinationPath: Path, teamNumber: Int) {
-    val text = destinationPath.readText()
-        .replace(teamNumberRegex, teamNumber.toString())
-    destinationPath.writeText(text)
+private fun chooseDestinationPath(): Path {
+    while (true) {
+        print("which path would you like to create the project in? ")
+        val path = Path.of(tildeExpand(readln()))
+        if (!path.exists()) {
+            path.createDirectory()
+            return path
+        }
+        if (!path.isDirectory()) {
+            println("that path isn't a directory!")
+            continue
+        }
+        if (Files.list(path).findAny().isPresent) {
+            println("that path contains files already!")
+            continue
+        }
+        return path
+    }
+}
+
+private fun chooseTeamNumber(): Int {
+    while (true) {
+        try {
+            print("what is your team number? ")
+            return readln().toInt()
+        } catch (e: NumberFormatException) {
+            println("invalid number")
+        }
+    }
 }
